ad09214ee6c1203adc0023fca37214b8c3781523
Evelyn Stender <emstender@live.com>
2018-09-02 14:36:18 -0300

    Arrumando configs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const config = require('config');
 const T = new Twit({
   consumer_key: config.twitter.consumerKey,
   consumer_secret: config.twitter.consumerSecret,
-  access_token: config.twitter.accessTokenKey,
+  access_token: config.twitter.accessToken,
   access_token_secret: config.twitter.accessTokenSecret,
 })
 
